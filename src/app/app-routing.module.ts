1a8a01981893407c5c0767b45c0674028585fdac
Alex Dair Carrillo Ortiz <alex.carrillo@sofka.com.co>
2022-10-22 11:12:36 -0500

    cancelando crud de asignaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { ConsultarAsignacionesPeriodoComponent } from "./componentes/asignaciones/periodo/consultar-asignaciones-periodo/consultar-asignaciones-periodo.component";
 
 import { InicioComponent } from "./componentes/inicio/inicio.component";
 import { ConsultarPeriodoComponent } from "./componentes/periodo/consultar-periodo/consultar-periodo.component";
@@ -9,7 +8,6 @@ import { ConsultarUsuarioComponent } from "./componentes/usuario/consultar-usuar
 import { RegistrarUsuarioComponent } from "./componentes/usuario/registrar-usuario/registrar-usuario.component";
 
 const routes: Routes = [
-    { path: 'consultar-asignaciones-periodo', component: ConsultarAsignacionesPeriodoComponent },
     { path: 'registrar-usuario', component: RegistrarUsuarioComponent },
     { path: 'consultar-usuario', component: ConsultarUsuarioComponent },
     { path: 'registrar-periodo', component: RegistrarPeriodoComponent },
--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,28 +1,26 @@
+
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 import { RoutingModule } from '../app-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 import { InicioComponent } from './inicio/inicio.component';
 import { MenuMainComponent } from './menu-main/menu-main.component';
 import { ConsultarPeriodoComponent } from './periodo/consultar-periodo/consultar-periodo.component';
-import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegistrarPeriodoComponent } from './periodo/registrar-periodo/registrar-periodo.component';
 import { ModalModificarPeriodoComponent } from './periodo/modal-modificar-periodo/modal-modificar-periodo.component';
-import { SharedModule } from '../shared/shared.module';
 import { RegistrarUsuarioComponent } from './usuario/registrar-usuario/registrar-usuario.component';
 import { ConsultarUsuarioComponent } from './usuario/consultar-usuario/consultar-usuario.component';
 import { ModalModificarUsuarioComponent } from './usuario/modal-modificar-usuario/modal-modificar-usuario.component';
-import { RegistrarAsignacionesPeriodoComponent } from './asignaciones/periodo/registrar-asignaciones-periodo/registrar-asignaciones-periodo.component';
-import { ConsultarAsignacionesPeriodoComponent } from './asignaciones/periodo/consultar-asignaciones-periodo/consultar-asignaciones-periodo.component';
-import { RegistrarAsignacionesCursoComponent } from './asignaciones/curso/registrar-asignaciones-curso/registrar-asignaciones-curso.component';
-import { ConsultarAsignacionesCursoComponent } from './asignaciones/curso/consultar-asignaciones-curso/consultar-asignaciones-curso.component';
 
 @NgModule({
   declarations: [
     InicioComponent, MenuMainComponent, ConsultarPeriodoComponent, LoginComponent, RegistrarPeriodoComponent, ModalModificarPeriodoComponent, 
-    RegistrarUsuarioComponent, ConsultarUsuarioComponent, ModalModificarUsuarioComponent, RegistrarAsignacionesPeriodoComponent, ConsultarAsignacionesPeriodoComponent, RegistrarAsignacionesCursoComponent, ConsultarAsignacionesCursoComponent
+    RegistrarUsuarioComponent, ConsultarUsuarioComponent, ModalModificarUsuarioComponent
   ],
   imports: [
     RoutingModule, CommonModule, FormsModule, ReactiveFormsModule, SharedModule
