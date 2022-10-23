dd75e07cff1cab75f67d8ba8d1afa02ebc176004
Alex Dair <lm_k215@hotmail.com>
2022-10-23 15:25:18 -0500

    registrar asignaciones listo, solo falta las alertas

--- src/app/componentes/asignaciones/registrar-asignacion/registrar-asignacion.component.ts
+++ src/app/componentes/asignaciones/registrar-asignacion/registrar-asignacion.component.ts
@@ -8,38 +8,99 @@ import { Constantes } from 'src/app/utils/constantes';
 })
 export class RegistrarAsignacionComponent implements OnInit {
 
-  tituloMaestro = "";
-  cursoSeleccionado = "";
+  styleSelect = "form-control sltTipoAsignacion";
+  styleBotonAsignar = "btn btn-primary btnAsignarDesactivado";
+  activarBotonAsignar = true;
+  maestroSeleccionado = "";
+  checkSeleccionado: any;
+  tituloMaestros = "";
+  maestros: string[] = [];
+  tituloAsignados = "";
+  asignados: string[] = [];
+  asignadosSeleccionados: string[] = [];
   validarSelecionTipoAsignacion = false;
   tiposAsignaciones = Constantes.TIPOS_ASIGNACIONES;
   tipoAsignacionSeleccionado = "Seleccionar";
-
-  maestros: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  seleccionarCurso(curso: string): void {
-    this.cursoSeleccionado = curso;
-    console.log(this.cursoSeleccionado);
+  limpiarPanels(): void {
+    this.styleSelect = "form-control sltTipoAsignacion";
+    this.styleBotonAsignar = "btn btn-primary btnAsignarDesactivado";
+    this.activarBotonAsignar = true;
+    this.maestroSeleccionado = "";
+    this.tituloMaestros = "";
+    this.maestros = [];
+    this.tituloAsignados = "";
+    this.asignados = [];
+    this.asignadosSeleccionados = [];
+    this.validarSelecionTipoAsignacion = false;
+    this.tiposAsignaciones = Constantes.TIPOS_ASIGNACIONES;
+    this.tipoAsignacionSeleccionado = "Seleccionar";
+  }
+
+  asignar(): void {
+    console.log("Maestro seleccionado -> " + this.maestroSeleccionado);
+    console.log("Asignados seleccionados -> " + this.asignadosSeleccionados);
+    this.limpiarPanels();
+  }
+
+  validarActivacionBotonAsignar(): void {
+    if(this.maestroSeleccionado != "" && this.asignadosSeleccionados.length > 0) {
+      this.activarBotonAsignar = false;
+      this.styleBotonAsignar = "btn btn-primary btnAsignar";
+    }
+  }
+
+  seleccionarMaestro(maestro: string): void {
+    this.maestroSeleccionado = maestro;
+    this.validarActivacionBotonAsignar();
+    console.log(this.maestroSeleccionado);
+  }
+
+  cargarAsignados(asignado: string): void {
+    this.cargarChecks(asignado);
+    if(this.checkSeleccionado.checked) {
+      this.asignadosSeleccionados.push(asignado);
+      console.log(asignado);
+    }
+    this.validarActivacionBotonAsignar();
+  }
+
+  cargarChecks(asignado: string): void {
+    this.checkSeleccionado = window.document.getElementById(asignado);
+  }
+
+  seleccionarAsignados(asignado: string): void {
+    this.cargarAsignados(asignado);
   }
 
   seleccionarTipoAsignacion(): void {
-    this.validarSelecionTipoAsignacion = true;
     switch (this.tipoAsignacionSeleccionado) {
       case 'Estudiante':
-        this.maestros = ["Diego","Mario","Lisa","Plinio","Marta"];
-      break;
-      case "Asignatura":
-        this.maestros = ["Mate","Religion","Fisica","Sociales","Deporte"];
-      break;
-      case "Curso":
-        this.maestros = ["1ro","2do","3ro","4to","5to"];    
-      break;
+        this.tituloMaestros = "Lista de Cursos";
+        this.maestros = Constantes.NOMBRES_PERIODO;
+        this.tituloAsignados = "Lista de Estudiantes";
+        this.asignados = ["Diego","Mario","Lisa","Plinio","Marta"];
+        break;
+        case "Asignatura":
+          this.tituloMaestros = "Lista de Cursos";
+          this.maestros = Constantes.NOMBRES_PERIODO;
+          this.tituloAsignados = "Lista de Asignaturas";
+          this.asignados = ["Mate","Religion","Fisica","Sociales","Deporte"];
+          break;
+          case "Curso":
+            this.tituloMaestros = "Lista de Periodos";
+            this.maestros = Constantes.NOMBRES_PERIODO;
+            this.tituloAsignados = "Lista de Cursos";
+            this.asignados = ["1ro","2do","3ro","4to","5to"];    
+            break;
     }
-    console.log(this.tipoAsignacionSeleccionado);
+    this.validarSelecionTipoAsignacion = true;
+    this.styleSelect = "form-control sltTipoAsignacionDesactivado";
   }
 
 }
